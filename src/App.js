130968da0e9a648f9520a37d9eb845d339bfd886
CrazyBolillo <40015596+CrazyBolillo@users.noreply.github.com>
2021-06-06 10:42:34 -0500

    Added title to appbar and logo drawer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import {
     AppBar, Box,
     Container,
-    createMuiTheme, Drawer,
+    createMuiTheme, Divider, Drawer,
     IconButton, List, ListItem, ListItemIcon, ListItemText,
     makeStyles,
     ThemeProvider,
@@ -13,18 +13,25 @@ import MenuIcon from '@material-ui/icons/Menu'
 import DashboardIcon from '@material-ui/icons/Dashboard'
 import CloudIcon from '@material-ui/icons/Cloud'
 import PowerIcon from '@material-ui/icons/Power'
-import {BrowserRouter, Route, Link, Redirect} from "react-router-dom"
+import {Route, Link, useLocation} from "react-router-dom"
 import Dashboard from "./pages/dashboard"
 import Ambient from "./pages/ambient"
 import Relays from "./pages/relays";
 
 const piplantTheme = createMuiTheme({})
-const useStyles = makeStyles((piplantTheme) => ({
+const useStyles = makeStyles(piplantTheme => ({
     menuIcon: {
         color: 'white'
     },
     offset: {
         minHeight: "64px"
+    },
+    logoContainer: {
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        padding: piplantTheme.spacing(2)
     },
     list: {
         width: 220
@@ -35,27 +42,24 @@ const useStyles = makeStyles((piplantTheme) => ({
     }
 }))
 
-const links = [
-    {
+const links = {
+    '/dashboard': {
         text: 'Dashboard',
-        link: '/dashboard',
         icon: DashboardIcon
     },
-    {
+    '/ambient': {
         text: 'Ambient',
-        link: '/ambient',
         icon: CloudIcon
     },
-    {
+    '/relays': {
         text: 'Relays',
-        link: '/relays',
         icon: PowerIcon
     }
-]
+}
 
 function DrawerLink(props) {
     return (
-        <Link to={props.data.link} className={props.className} onClick={props.onClick}>
+        <Link to={props.to} className={props.className} onClick={props.onClick}>
             <ListItem button>
                 <ListItemIcon>
                     <props.data.icon/>
@@ -71,44 +75,47 @@ function App() {
     const [state, setState] = React.useState(false)
 
     return (
-        <BrowserRouter>
-            <ThemeProvider theme={piplantTheme}>
-                <Container>
-                    <AppBar>
-                        <Toolbar>
-                            <IconButton onClick={() => setState(!state)}>
-                                <MenuIcon className={classes.menuIcon}/>
-                            </IconButton>
-                            <Typography variant="h6">
-                                Piplant
-                            </Typography>
-                        </Toolbar>
-                    </AppBar>
-                    <Box className={classes.offset}/>
-                    <Route path="/dashboard">
-                        <Dashboard/>
-                    </Route>
-                    <Route path="/ambient">
-                        <Ambient/>
-                    </Route>
-                    <Route path="/relays">
-                        <Relays/>
-                    </Route>
-                </Container>
-                <Drawer open={state} onClose={() => setState(false)}>
-                    <Box>
-                        <List className={classes.list} component="nav">
-                            {
-                                links.map(entry => (
-                                    <DrawerLink className={classes.link} data={entry} key={entry.link}
-                                                onClick={() => {setState(false)}}/>
-                                ))
-                            }
-                        </List>
+        <ThemeProvider theme={piplantTheme}>
+            <Container>
+                <AppBar>
+                    <Toolbar>
+                        <IconButton onClick={() => setState(!state)}>
+                            <MenuIcon className={classes.menuIcon}/>
+                        </IconButton>
+                        <Typography variant="h6">
+                            {links[useLocation().pathname].text}
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Box className={classes.offset}/>
+                <Route path="/dashboard">
+                    <Dashboard/>
+                </Route>
+                <Route path="/ambient">
+                    <Ambient/>
+                </Route>
+                <Route path="/relays">
+                    <Relays/>
+                </Route>
+            </Container>
+            <Drawer open={state} onClose={() => setState(false)}>
+                <Box>
+                    <Box className={classes.logoContainer}>
+                        <Typography variant="h5">Piplant</Typography>
+                        <Typography variant="h6">🌱</Typography>
                     </Box>
-                </Drawer>
-            </ThemeProvider>
-        </BrowserRouter>
+                    <Divider/>
+                    <List className={classes.list} component="nav">
+                        {
+                            Object.keys(links).map(key => (
+                                <DrawerLink className={classes.link} data={links[key]} key={key} to={key}
+                                            onClick={() => {setState(false)}}/>
+                            ))
+                        }
+                    </List>
+                </Box>
+            </Drawer>
+        </ThemeProvider>
     );
 }
 
